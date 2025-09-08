0b61b8b694188c0af5a684d2db5b1e2c3491f536
otsuka0728 <rhanata593@gmail.com>
2025-09-08 15:27:16 +0900

    Playerkaeta

--- OWProjct (1)/Project/GameProject/Game/Player.h
+++ OWProjct (1)/Project/GameProject/Game/Player.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "../Base/Base.h"
+
+class Player :public Base {
+public:
+	CImage m_img;
+public:
+	Player(const CVector2D& pos);
+	void Draw();
+	void Update();
+	void Collision(Base* b);
+
+	TexAnimData _anim_data[];
+};
