5bb079bba2918eda83d668e3117fd00b6ba69742
otsuka0728 <rhanata593@gmail.com>
2025-09-08 18:53:24 +0900

    Player.cppwoskosikuwaemasita

--- OWProjct (1)/Project/GameProject/Game/Player.cpp
+++ OWProjct (1)/Project/GameProject/Game/Player.cpp
@@ -25,20 +25,42 @@ static TexAnim _anim_attack[]{
 	{13,4},
 	{14,4}
 };
+static TexAnim _anim_deth[]{
+	{0,4},
+	{1,4},
+	{2,4},
+	{3,4},
+	{4,4},
+	{5,4},
+	{6,4},
+	{7,4}
+};
+static TexAnim _anim_hurt[]{
+	{0,4},
+	{1,4},
+	{2,4},
+	{3,4}
+};
 TexAnimData _anim_data[]{
-	ANIMDATA(_anim_idle),
+	ANIMDATA(_anim_idle),ANIMDATA(_anim_attack),ANIMDATA(_anim_deth),ANIMDATA(_anim_hurt),
 };
 Player::Player(const CVector2D& pos)
 	:Base(eType_Player)
 {
+	m_img = COPY_RESOURCE("Player", CImage);
+	m_img.ChangeAnimation(0);
+	m_pos = pos;
 }
 
 void Player::Draw()
 {
+	m_img.SetPos(GetScreenPos(m_pos));
+	m_img.Draw();
 }
 
 void Player::Update()
 {
+	m_img.UpdateAnimation();
 }
 
 void Player::Collision(Base* b)
