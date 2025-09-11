6c0a69d995666831b538d3f8b6bc114e808792a7
矢 <24051910@d.anabuki-college.net>
2025-09-11 12:29:27 +0900

    n

--- OWProjct (1)/Project/GameProject/UI/UIBlock.cpp
+++ OWProjct (1)/Project/GameProject/UI/UIBlock.cpp
@@ -4,12 +4,14 @@ UIBlock::UIBlock()
 	:Base(eType_UI)
 {
 	m_img[0] = COPY_RESOURCE("O_block", CImage);
-	m_img[1] = COPY_RESOURCE("S_block", CImage);
-	m_img[2] = COPY_RESOURCE("Z_block", CImage);
-	m_img[3] = COPY_RESOURCE("J_block", CImage);
-	m_img[4] = COPY_RESOURCE("L_block", CImage);
-	m_img[5] = COPY_RESOURCE("T_block", CImage);
-	m_img[6] = COPY_RESOURCE("I_block", CImage);
+	m_img[1] = COPY_RESOURCE("I_block", CImage);
+	m_img[2] = COPY_RESOURCE("T_block", CImage);
+	m_img[3] = COPY_RESOURCE("Z_block", CImage);
+	m_img[4] = COPY_RESOURCE("S_block", CImage);
+	m_img[5] = COPY_RESOURCE("L_block", CImage);
+	m_img[6] = COPY_RESOURCE("J_block", CImage);
+	
+	
 
 	m_number[0] = COPY_RESOURCE("1", CImage);
 	m_number[1] = COPY_RESOURCE("2", CImage);
@@ -28,7 +30,7 @@ void UIBlock::Draw()
 {
 	for (int i = 0; i < 7; i++)
 	{
-		m_img[i].SetPos(-50 +300 * i , 750);
+		m_img[i].SetPos(0 +270 * i , 750);
 		m_number[i].SetPos(0 + 300 * i, 1000);
 		m_img[i].Draw();
 		m_number[i].Draw();
